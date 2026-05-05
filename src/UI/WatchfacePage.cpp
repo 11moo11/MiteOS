390bb570f795dbce412f1f1e040aba03cc2c0af5
Nicholas <nicholas.mulholland2@gmail.com>
2026-05-05 00:07:39 -0500

    Working

--- src/UI/WatchfacePage.cpp
+++ src/UI/WatchfacePage.cpp
@@ -1,18 +1,18 @@
 #include "WatchfacePage.h"
 #include "../MiteOS.h"
 
-#include "Watchfaces/Watchface.h"
 #include "Watchfaces/MiteoWatchface.h"
-#include "Watchfaces/SEG7Watchface.h"
-#include "Watchfaces/BTTFWatchface.h"
-#include "Watchfaces/PokemonWatchface.h"
-#include "Watchfaces/MachPaintWatchface.h"
+#include "Watchfaces/Watchface.h"
+// #include "Watchfaces/SEG7Watchface.h"
 #include "Watchfaces/AnalogWatchface.h"
-#include "Watchfaces/HobbitTimeWatchface.h"
+#include "Watchfaces/BTTFWatchface.h"
+#include "Watchfaces/MachPaintWatchface.h"
+#include "Watchfaces/PokemonWatchface.h"
+// #include "Watchfaces/HobbitTimeWatchface.h"
 #include "Watchfaces/CalendarWatchface.h"
-#include "Watchfaces/TrainWatchface.h"
+// #include "Watchfaces/TrainWatchface.h"
 #include "Watchfaces/BadForEyeWatchface.h"
-#include "Watchfaces/TetrisWatchface.h"
+// #include "Watchfaces/TetrisWatchface.h"
 #include "Watchfaces/StarryHorizonWatchface.h"
 
 RTC_DATA_ATTR uint8_t watchFaceId = 0;
@@ -20,61 +20,54 @@ RTC_DATA_ATTR uint8_t actionTopLeft = GLOBAL_PAGE_SETTINGS;
 RTC_DATA_ATTR uint8_t actionTopRight = GLOBAL_PAGE_NOTIFICATIONS;
 
 PROGMEM MiteoWatchface miteoWatchface;
-PROGMEM SEG7Watchface seg7Watchface;
+// PROGMEM SEG7Watchface seg7Watchface;
 PROGMEM BTTFWatchface bttfWatchface;
 PROGMEM PokemonWatchface pokemonWatchface;
 PROGMEM MacPaintWatchface macPaintWatchface;
 PROGMEM AnalogWatchface analogWatchface;
-PROGMEM HobbitTimeWatchface hobbitTimeWatchface;
+// PROGMEM HobbitTimeWatchface hobbitTimeWatchface;
 PROGMEM CalendarWatchface calendarWatchface;
-PROGMEM TrainWatchface trainWatchface;
+// PROGMEM TrainWatchface trainWatchface;
 PROGMEM BadForEyeWatchface badForEyeWatchface;
-PROGMEM TetrisWatchface tetrisWatchface;
+// PROGMEM TetrisWatchface tetrisWatchface;
 PROGMEM StarryHorizonWatchface starryHorizonWatchface;
-PROGMEM Watchface* watchfaces[] = {
-	&miteoWatchface,
-	&seg7Watchface,
-	&bttfWatchface,
-	&pokemonWatchface,
-	&macPaintWatchface,
-	&analogWatchface,
-	&hobbitTimeWatchface,
-	&calendarWatchface,
-	&trainWatchface,
-	&badForEyeWatchface,
-	&tetrisWatchface,
-	&starryHorizonWatchface,
-	// DONT FORGET TO INCREATE WATCHFACE_COUNT IN HEADER FILE
+PROGMEM Watchface *watchfaces[] = {
+    &miteoWatchface,      &seg7Watchface,     &bttfWatchface,
+    &pokemonWatchface,    &macPaintWatchface, &analogWatchface,
+    &hobbitTimeWatchface, &calendarWatchface, &trainWatchface,
+    &badForEyeWatchface,  &tetrisWatchface,   &starryHorizonWatchface,
+    // DONT FORGET TO INCREATE WATCHFACE_COUNT IN HEADER FILE
 };
 
 void WatchfacePage::drawPage() {
-	printDebug("Drawing Watchface");
-	
-	mDisplay.fillScreen(BACKGROUND_COLOR);
-	mDisplay.setTextColor(FOREGROUND_COLOR);
-	
-	watchfaces[watchFaceId]->draw();
+  printDebug("Drawing Watchface");
+
+  mDisplay.fillScreen(BACKGROUND_COLOR);
+  mDisplay.setTextColor(FOREGROUND_COLOR);
+
+  watchfaces[watchFaceId]->draw();
 }
 
 bool WatchfacePage::onButtonPressed(uint8_t buttonIndex) {
-	if(buttonIndex == BTN_BACK) {
-		PageManager::showPage(actionTopLeft);
-		return true;
-	}
-	
-	if(buttonIndex == BTN_UP) {
-		PageManager::showPage(actionTopRight);
-		return true;
-	}
-	
-	if(buttonIndex == BTN_MENU) {
-		PageManager::showPage(GLOBAL_PAGE_APPS);
-		return true;
-	}
-	return false;
+  if (buttonIndex == BTN_BACK) {
+    PageManager::showPage(actionTopLeft);
+    return true;
+  }
+
+  if (buttonIndex == BTN_UP) {
+    PageManager::showPage(actionTopRight);
+    return true;
+  }
+
+  if (buttonIndex == BTN_MENU) {
+    PageManager::showPage(GLOBAL_PAGE_APPS);
+    return true;
+  }
+  return false;
 }
 
-Watchface* WatchfacePage::getWatchface(uint8_t index) {
-	if(index > WATCHFACE_COUNT) return watchfaces[0];
-	return watchfaces[index];
+Watchface *WatchfacePage::getWatchface(uint8_t index) {
+  if (index > WATCHFACE_COUNT)
+    return watchfaces[0];
+  return watchfaces[index];
 }
--- src/UI/Watchfaces/BTTFWatchface.cpp
+++ src/UI/Watchfaces/BTTFWatchface.cpp
@@ -6,89 +6,83 @@
 
 // For more fonts look here:
 // https://learn.adafruit.com/adafruit-gfx-graphics-library/using-fonts
-//#define FONT_LARGE       Bohemian_Typewriter22pt7b
-//#define FONT_MEDUM       Bohemian_Typewriter18pt7b
-#define FONT_7_SEG_LARGE    DSEG7_Classic_Bold_25
-#define FONT_7_SEG_MEDIUM   Seven_Seg18pt7b
+// #define FONT_LARGE       Bohemian_Typewriter22pt7b
+// #define FONT_MEDUM       Bohemian_Typewriter18pt7b
+#define FONT_7_SEG_LARGE DSEG7_Classic_Bold_25
+#define FONT_7_SEG_MEDIUM Seven_Seg18pt7b
 
 #define YEAR_OFFSET 10
 
-void BTTFWatchface::draw(){
-	mDisplay.drawBitmap(0, 0, bttf, 200, 200, FOREGROUND_COLOR);
-	drawTime();
-	drawDate();
-	drawSteps();
-	drawBattery();
+void BTTFWatchface::draw() {
+  mDisplay.drawBitmap(0, 0, bttf, 200, 200, FOREGROUND_COLOR);
+  drawTime();
+  drawDate();
+  drawSteps();
+  drawBattery();
 }
 
+void BTTFWatchface::printCentered(uint16_t x, uint16_t y, String text) {
+  int16_t x1, y1;
+  uint16_t w, h;
 
-void BTTFWatchface::printCentered(uint16_t x, uint16_t y, String text){
-	int16_t  x1, y1;
-	uint16_t w, h;
-
-	mDisplay.getTextBounds(text, 40, 100, &x1, &y1, &w, &h);
-	mDisplay.setCursor(x-w/2, y+h/2);
-	mDisplay.println(text);
+  mDisplay.getTextBounds(text, 40, 100, &x1, &y1, &w, &h);
+  mDisplay.setCursor(x - w / 2, y + h / 2);
+  mDisplay.println(text);
 }
 
+void BTTFWatchface::drawTime() {
+  mDisplay.setFont(&FONT_7_SEG_LARGE);
+  mDisplay.setTextColor(BACKGROUND_COLOR);
 
-void BTTFWatchface::drawTime(){
-	mDisplay.setFont(&FONT_7_SEG_LARGE);
-	mDisplay.setTextColor(BACKGROUND_COLOR);
+  String hourStr = String(MiteOS::currentTime.Hour);
+  hourStr = MiteOS::currentTime.Hour < 10 ? "0" + hourStr : hourStr;
+  printCentered(101, 42, hourStr);
 
-	String hourStr = String(MiteOS::currentTime.Hour);
-	hourStr = MiteOS::currentTime.Hour < 10 ? "0" + hourStr : hourStr;
-	printCentered(101, 42, hourStr);
-
-	String minStr = String(MiteOS::currentTime.Minute);
-	minStr = MiteOS::currentTime.Minute < 10 ? "0" + minStr : minStr;
-	printCentered(165, 42, minStr);
+  String minStr = String(MiteOS::currentTime.Minute);
+  minStr = MiteOS::currentTime.Minute < 10 ? "0" + minStr : minStr;
+  printCentered(165, 42, minStr);
 }
 
+void BTTFWatchface::drawDate() {
+  mDisplay.setFont(&FONT_7_SEG_MEDIUM);
+  mDisplay.setTextColor(BACKGROUND_COLOR);
 
-void BTTFWatchface::drawDate(){
-	mDisplay.setFont(&FONT_7_SEG_MEDIUM);
-	mDisplay.setTextColor(BACKGROUND_COLOR);
+  String month = Lang::monthShortStr(MiteOS::currentTime.Month);
+  printCentered(35, 103, month);
 
-	String month = Lang::monthShortStr(MiteOS::currentTime.Month);
-	printCentered(35, 103, month);
+  String dayStr = String(MiteOS::currentTime.Day);
+  dayStr = MiteOS::currentTime.Day < 10 ? "0" + dayStr : dayStr;
+  printCentered(94, 103, dayStr);
 
-	String dayStr = String(MiteOS::currentTime.Day);
-	dayStr = MiteOS::currentTime.Day < 10 ? "0" + dayStr : dayStr;
-	printCentered(94, 103, dayStr);
-
-	//if(alarm_timer < 0){
-		String yearStr = String(1970 + MiteOS::currentTime.Year + YEAR_OFFSET);
-		printCentered(157, 103, yearStr);
-	//} else {
-	//    String alarm_str = "T-" + String(alarm_timer);
-	//    printCentered(157, 103, alarm_str);
-	//}
+  // if(alarm_timer < 0){
+  String yearStr = String(1970 + MiteOS::currentTime.Year + YEAR_OFFSET);
+  printCentered(157, 103, yearStr);
+  //} else {
+  //    String alarm_str = "T-" + String(alarm_timer);
+  //    printCentered(157, 103, alarm_str);
+  //}
 }
 
+void BTTFWatchface::drawBattery() {
+  mDisplay.setFont(&FONT_7_SEG_MEDIUM);
+  mDisplay.setTextColor(BACKGROUND_COLOR);
 
-void BTTFWatchface::drawBattery(){
-	mDisplay.setFont(&FONT_7_SEG_MEDIUM);
-	mDisplay.setTextColor(BACKGROUND_COLOR);
-
-	
-	int8_t bat = PowerManager::getBatteryPercentage();
-	String batStr = String(bat);
-	batStr = bat < 10 ? "0" + batStr : batStr;
-	batStr = bat < 100 ? "0" + batStr : batStr;
-	printCentered(151, 169, batStr);
+  int8_t bat = PowerManager::getBatteryPercentage();
+  String batStr = String(bat);
+  batStr = bat < 10 ? "0" + batStr : batStr;
+  batStr = bat < 100 ? "0" + batStr : batStr;
+  printCentered(151, 169, batStr);
 }
 
+void BTTFWatchface::drawSteps() {
+  mDisplay.setFont(&FONT_7_SEG_MEDIUM);
+  mDisplay.setTextColor(BACKGROUND_COLOR);
 
-void BTTFWatchface::drawSteps(){
-	mDisplay.setFont(&FONT_7_SEG_MEDIUM);
-	mDisplay.setTextColor(BACKGROUND_COLOR);
+  uint32_t steps = ActivityManager::getStepCount();
+  String stepStr = String(steps);
+  for (int i = 1; i < 5; i++) {
+    stepStr = steps < pow(10, i) ? "0" + stepStr : stepStr;
+  }
 
-	uint32_t steps = ActivityManager::getStepCount();
-	String stepStr = String(steps);
-	for(int i=1; i<5; i++){
-		stepStr = steps < pow(10, i) ? "0" + stepStr : stepStr;
-	}
-
-	printCentered(62, 169, stepStr);
+  printCentered(62, 169, stepStr);
 }
